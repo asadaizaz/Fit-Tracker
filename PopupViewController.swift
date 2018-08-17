b81d206cfff723c17d558d73eeac6333400c97a8
Asad Aizaz <asad.aizaz99@gmail.com>
2018-08-16 20:01:43 -0700

    Changed UI elements. Added basic functionality to the add exercise button.

---
+++ PopupViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PopupViewController.swift
+//  Fit-Tracker
+//
+//  Created by Asad Aizaz on 2018-08-16.
+//  Copyright © 2018 Asad Aizaz. All rights reserved.
+//
+
+import UIKit
+
+class PopupViewController: UIViewController {
+    
+   
+    
+    
+    
+    
+   
+
+}
